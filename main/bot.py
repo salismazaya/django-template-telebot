7d1bcfb9780d173bd511f112a43aedd5033edd86
salismazaya <salismazaya@gmail.com>
2022-10-11 20:57:01 +0700

    improv

--- main/bot.py
+++ main/bot.py
@@ -1,5 +1,4 @@
-from email import message
 from django.conf import settings
 from telebot import TeleBot
 
-bot = TeleBot(settings.TELEGRAM_BOT_TOKEN, threaded = False)
+bot = TeleBot(settings.TELEGRAM_BOT_TOKEN, threaded = not settings.DEBUG)
